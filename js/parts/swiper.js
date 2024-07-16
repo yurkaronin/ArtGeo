9ea3788147df45592c50d0ce973682a21d17a250
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-07-16 14:48:59 +0300

    слайдер с галереей фото + адаптив

--- js/parts/swiper.js
+++ js/parts/swiper.js
@@ -4,21 +4,32 @@ document.addEventListener('DOMContentLoaded', function () {
   if(document.querySelector('.gallery')) {
     const swiper = new Swiper(".gallery .swiper", {
       slidesPerView: 'auto',
-      spaceBetween: 32,
+      spaceBetween: 20,
       loop: true,
-      freeMode: true,
       autoplay: {
         delay: 25000,
         disableOnInteraction: false,
       },
       pagination: {
         el: ".gallery .swiper-pagination",
-        clickable: true,
+        type: "fraction",
       },
       navigation: {
         nextEl: ".gallery .swiper-button-next",
         prevEl: ".gallery .swiper-button-prev",
-      }
+      },
+      breakpoints: {
+        320: {
+          spaceBetween: 20,
+        },
+
+        768: {
+          spaceBetween: 24,
+        },
+        1024: {
+          spaceBetween: 32,
+        },
+      },
     });
 
   };
